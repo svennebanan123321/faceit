53adb5a0dd6f8db38d9c9dd8fcf798e9e7401c39
svennebanan123321 <asvenson0107@gmail.com>
2025-02-08 02:20:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,18 @@
-/* Show Video Function - Triggers Fullscreen & Sound on Click */
-function showVideo() {
-    var video = document.getElementById('video');
-    var container = document.getElementById('container');
-
-    video.style.display = 'block';
-    container.style.display = 'none';
-
-    enableAudio(); // Ensure sound plays
-    requestFullscreen();
+.clickable-image {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: black; /* In case the image doesn't fully cover */
 }
 
-/* Ensure Video Sound is Enabled */
-function enableAudio() {
-    var video = document.getElementById('video');
-
-    video.muted = false;
-    video.play().catch(() => {
-        document.addEventListener("click", enableAudio, { once: true });
-    });
-}
-
-/* Fullscreen Activation */
-function requestFullscreen() {
-    var content = document.getElementById('container-video');
-
-    if (content.requestFullscreen) {
-        content.requestFullscreen();
-    } else if (content.mozRequestFullScreen) { // Firefox
-        content.mozRequestFullScreen();
-    } else if (content.webkitRequestFullscreen) { // Chrome, Safari, Opera
-        content.webkitRequestFullscreen();
-    } else if (content.msRequestFullscreen) { // Internet Explorer/Edge
-        content.msRequestFullscreen();
-    }
+.clickable-image img {
+    width: 100vw;
+    height: 100vh;
+    object-fit: cover;
+    cursor: pointer;
 }
